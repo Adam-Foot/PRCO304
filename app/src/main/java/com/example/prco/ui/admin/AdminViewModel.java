abd5183c9435ddcfb57266b89ec5890a76772570
Adam-Foot <adam.foot@students.plymouth.ac.uk>
2020-02-04 19:30:12 +0000

    Created admin fragment and viewmodel classes

--- app/src/main/java/com/example/prco/ui/admin/AdminViewModel.java
+++ app/src/main/java/com/example/prco/ui/admin/AdminViewModel.java
@@ -1,4 +1,19 @@
 package com.example.prco.ui.admin;
 
-public class AdminViewModel {
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class AdminViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public AdminViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("Admin");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
 }
