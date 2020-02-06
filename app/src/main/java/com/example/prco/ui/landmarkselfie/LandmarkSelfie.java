8c69b35e6e253b64b57c1474a6abeb54f22b6453
Adam-Foot <adam.foot@students.plymouth.ac.uk>
2020-02-06 12:22:57 +0000

    Added blank selfie with a landmark activity

---
+++ app/src/main/java/com/example/prco/ui/landmarkselfie/LandmarkSelfie.java
@@ -0,0 +1,16 @@
+package com.example.prco.ui.landmarkselfie;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.prco.R;
+
+public class LandmarkSelfie extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_landmark_selfie);
+    }
+}
