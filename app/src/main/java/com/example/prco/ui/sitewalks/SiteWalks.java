d8d819f093778f1cef02df7ac0957ab542cb3a61
Adam-Foot <adam.foot@students.plymouth.ac.uk>
2020-02-06 12:14:02 +0000

    Created Site Walks activity and added it to the Manifest

---
+++ app/src/main/java/com/example/prco/ui/sitewalks/SiteWalks.java
@@ -0,0 +1,17 @@
+package com.example.prco.ui.sitewalks;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.prco.R;
+
+public class SiteWalks extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setTitle("Site Walks");
+        setContentView(R.layout.activity_site_walks);
+    }
+}
