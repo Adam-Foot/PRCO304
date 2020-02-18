9c855b7ad142ec0bced0661409f9fab00be987ee
Adam-Foot <adam.foot@students.plymouth.ac.uk>
2020-02-18 15:08:37 +0000

    Created Sites class to handle getter and setter for Firestore data

---
+++ app/src/main/java/com/example/prco/ui/sites/Sites.java
@@ -0,0 +1,38 @@
+package com.example.prco.ui.sites;
+
+public class Sites {
+
+    private String site_name, site_locationLat, site_locationLong;
+
+    public Sites() {}
+
+    public Sites(String site_name, String site_locationLat, String site_locationLong) {
+        this.site_name = site_name;
+        this.site_locationLat = site_locationLat;
+        this.site_locationLong = site_locationLong;
+    }
+
+    public String getSite_name() {
+        return site_name;
+    }
+
+    public void setSite_name(String site_name) {
+        this.site_name = site_name;
+    }
+
+    public String getSite_locationLat() {
+        return site_locationLat;
+    }
+
+    public void setSite_locationLat(String site_locationLat) {
+        this.site_locationLat = site_locationLat;
+    }
+
+    public String getSite_locationLong() {
+        return site_locationLong;
+    }
+
+    public void setSite_locationLong(String site_locationLong) {
+        this.site_locationLong = site_locationLong;
+    }
+}
