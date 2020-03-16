24fd5187b629c6d20b3ead71612f1cd730686aa4
Adam-Foot <adam.foot@students.plymouth.ac.uk>
2020-03-16 10:27:34 +0000

    Commented out animation data as it was causing errors

--- app/src/main/java/com/example/prco/ui/arcamera/ArCamera.java
+++ app/src/main/java/com/example/prco/ui/arcamera/ArCamera.java
@@ -114,11 +114,11 @@ public class ArCamera extends AppCompatActivity {
                 }));
 
 //         Animation Data for Andy OBJ. To be tested when Google update their Sceneform Plugin.
-        AnimationData danceData = modelRenderable.getAnimationData("andy_dance");
-        danceData.getName();
-        ModelAnimator andyAnimator = new ModelAnimator(danceData, modelRenderable);
-        andyAnimator.start();
-        andyAnimator.setRepeatCount(50);
+//        AnimationData danceData = modelRenderable.getAnimationData("andy_dance");
+//        danceData.getName();
+//        ModelAnimator andyAnimator = new ModelAnimator(danceData, modelRenderable);
+//        andyAnimator.start();
+//        andyAnimator.setRepeatCount(50);
     }
 
     private void addNodeToScene(ArFragment fragment, Anchor anchor, Renderable renderable){
