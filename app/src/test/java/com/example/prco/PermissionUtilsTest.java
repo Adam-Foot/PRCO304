9f38db1b70e19fe07a6dad38280dafc689e42710
Adam-Foot <adamfoot16@gmail.com>
2020-04-26 16:10:58 +0100

    Added testing files

---
+++ app/src/test/java/com/example/prco/PermissionUtilsTest.java
@@ -0,0 +1,16 @@
+package com.example.prco;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PermissionUtilsTest {
+
+    @Test
+    public void requestPermission() {
+    }
+
+    @Test
+    public void isPermissionGranted() {
+    }
+}
